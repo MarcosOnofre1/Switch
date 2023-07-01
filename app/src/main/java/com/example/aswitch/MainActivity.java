32b738fb03ebeaf50b531fd1e34514d13c243d03
MarcosOnofre1 <marcosxrzz@gmail.com>
2023-07-01 18:41:31 -0300

    Start commit!

---
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwitchMaterial swNotificacoes = findViewById(R.id.swNotificacoes);
+        TextView textNotificacoes = findViewById(R.id.textNotificacoes);
+
+        swNotificacoes.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                textNotificacoes.setText("ON");
+            } else {
+                textNotificacoes.setText("OFF");
+            }
+        });
+
+
+    }
+}
